77738afbe7e001dbdd8c8a45e239f94fd7b3145e
terefs <d.van.zurk@gmail.com>
2018-12-11 13:22:55 +0100

    navbar functionality on all devices

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,19 @@
 const navToggle = document.querySelector(".menu-button")
+const nav = document.querySelector('nav')
 
-navToggle.addEventListener('click', _ => {
+function openNav() {
+    console.log('openNav')
     document.body.classList.toggle('nav-is-open')
-})
+    
+}
+
+function closeNav() {
+    console.log('closeNav')
+    document.body.classList.remove('nav-is-open')
+}
+
+// navToggle.addEventListener('touchstart', openNav)
+navToggle.addEventListener('click', openNav)
+
+// nav.addEventListener('touchstart', closeNav)
+nav.addEventListener('click', closeNav)
